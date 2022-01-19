5a61e78383c34c7fe9e61168b7bc20489d5694a7
Julian-Liu01 <julian9051mk2@gmail.com>
2022-01-19 17:45:46 +0800

    011902

--- personal/js/demo.js
+++ personal/js/demo.js
@@ -1,20 +1,5 @@
 var oldScrollTop = 0;
-var slideShowCounter = 2;
-var zIndexCounter = -1;
 window.onload = function() {
-      setInterval(function() {
-        $('div').eq(slideShowCounter).animate({'left': '-50px'}, 500, function(){
-             $(this).css({'z-index': zIndexCounter, 'left': 0});
-            if(slideShowCounter - 1 < 0) {
-                 slideShowCounter = 2;
-            }else {
-                 slideShowCounter -= 1;
-            }
-            $('div').eq(slideShowCounter).css({'z-index': 999});
-            zIndexCounter--;
-        });
-    }, 2000);
-
     $(window).on('scroll', function() {
       let scrollTop = $(window).scrollTop();
       let windowHeight = $(window).height();
